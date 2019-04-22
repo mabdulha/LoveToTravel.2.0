c7e3e32b30d8c762c8ed567ad44e8727a8b07f46
mozeeb <mozeebabdulha@gmail.com>
2019-04-22 21:29:21 +0100

    Created Google Authentication Layout

---
+++ app/src/main/java/ie/com/lovetotravel20/authentication/GoogleAuthentication.java
@@ -0,0 +1,16 @@
+package ie.com.lovetotravel20.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.com.lovetotravel20.R;
+
+public class GoogleAuthentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_authentication);
+
+    }
+}
