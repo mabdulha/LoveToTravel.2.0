9ad14d854f681a9a1ed693206cb2b3b08fc9d027
mozeeb <mozeebabdulha@gmail.com>
2019-04-28 13:28:33 +0100

    Code Clean up

--- app/src/main/java/ie/com/lovetotravel20/activities/GoogleMaps.java
+++ app/src/main/java/ie/com/lovetotravel20/activities/GoogleMaps.java
@@ -3,7 +3,6 @@ package ie.com.lovetotravel20.activities;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
